7f8bd4b32dceee025a80f0b3f3cd3e2cf96888d7
Kévin Paris <kevin.paris701@gmail.com>
2021-06-19 23:43:50 +0200

    [WIP] display info on battle screen

--- src/popup.js
+++ src/popup.js
@@ -1,35 +1,46 @@
+const createDiv = (elo) => {
+	let div = document.createElement("div")
+	div.setAttribute("id", "showdownLadderInfos");
+	let strong1 = document.createElement("strong")
+	let strong2 = document.createElement("strong")
+	strong1.innerText = getLadderName()
+	strong2.innerText = elo
+	div.append(strong1)
+	div.append(document.createElement("br"))
+	div.append(strong2)
+	return div
+}
+
 const getLadderName = () => {
-    const url = window.location.href
-    const regex = RegExp('\/battle-(\\w*)-', 'gm')
-    return regex.exec(url)[1]
+	const url = window.location.href
+	const regex = RegExp('\/battle-(\\w*)-', 'gm')
+	return regex.exec(url)[1]
 }
 
-const getLadderRank = async () => {
-    const username = document.cookie
-    .split('; ')
-    .find(row => row.startsWith('showdown_username='))
-    .split('=')[1];
+const getLadderRank = () => {
+	console.log("still inside")
+	const username = document.cookie
+	.split('; ')
+	.find(row => row.startsWith('showdown_username='))
+	.split('=')[1];
 
-    chrome.runtime.sendMessage( //goes to bg_page.js
-        `https://pokemonshowdown.com/users/${username}`,
-        html => {
-            const regex = RegExp(`<td>${getLadderName()}<\\/td><td style=\"text-align:center\"><strong>(\\d*)<\\/strong>`, "gm")
-            const elo = regex.exec(html)[1]
-            alert(`Ladder : ${getLadderName()}\nElo : ${elo}`)
-        }
-    );
+	chrome.runtime.sendMessage( //goes to bg_page.js
+		`https://pokemonshowdown.com/users/${username}`,
+		html => {
+			const regex = RegExp(`<td>${getLadderName()}<\\/td><td style=\"text-align:center\"><strong>(\\d*)<\\/strong>`, "gm")
+			const elo = regex.exec(html)[1]
+
+			const div = createDiv(elo)
+			const leftbar = document.getElementsByClassName("leftbar")[0]
+			leftbar.appendChild(div);
+			//alert(`Ladder : ${getLadderName()}\nElo : ${elo}`)
+		}
+	);
 }
 
-let battle = false
 setInterval(() => {
-
-    if (window.location.href !== "https://play.pokemonshowdown.com/" && battle == false) {
-        battle = true
-        getLadderRank()
-    }
-    else if (window.location.href === "https://play.pokemonshowdown.com/" && battle == true) {
-        battle = false
-    }
-}, 3000)
-
-
+	const customElem = document.getElementById("showdownLadderInfos")
+	if (customElem === null && window.location.href !== "https://play.pokemonshowdown.com/") {
+		getLadderRank()
+	}
+}, 1000)
