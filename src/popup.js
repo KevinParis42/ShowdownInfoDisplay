b77e290c044099f7a8d18af7976da12dd82021a7
Kévin Paris <kevin.paris701@gmail.com>
2021-06-20 16:04:32 +0200

    [WIP] cleaner display + bug resolve

--- src/popup.js
+++ src/popup.js
@@ -1,9 +1,10 @@
 const createDiv = (elo) => {
 	let div = document.createElement("div")
+	div.style = "font-size:12px;padding:5px;"
 	div.setAttribute("id", "showdownLadderInfos");
 	let strong1 = document.createElement("strong")
 	let strong2 = document.createElement("strong")
-	strong1.innerText = getLadderName()
+	strong1.innerText = getLadderName().replace("battle", "")
 	strong2.innerText = elo
 	div.append(strong1)
 	div.append(document.createElement("br"))
@@ -11,36 +12,43 @@ const createDiv = (elo) => {
 	return div
 }
 
-const getLadderName = () => {
-	const url = window.location.href
-	const regex = RegExp('\/battle-(\\w*)-', 'gm')
-	return regex.exec(url)[1]
-}
-
-const getLadderRank = () => {
-	console.log("still inside")
-	const username = document.cookie
+const getUserName = () => {
+	return document.cookie
 	.split('; ')
 	.find(row => row.startsWith('showdown_username='))
 	.split('=')[1];
+}
 
-	chrome.runtime.sendMessage( //goes to bg_page.js
-		`https://pokemonshowdown.com/users/${username}`,
-		html => {
-			const regex = RegExp(`<td>${getLadderName()}<\\/td><td style=\"text-align:center\"><strong>(\\d*)<\\/strong>`, "gm")
-			const elo = regex.exec(html)[1]
+const getLadderName = () => {
+	const url = window.location.href
+	const regex = RegExp('\/battle-(\\w*)-', 'gm')
+	const res = regex.exec(url)
+	return !res ? "unknown" : res[1]
+}
 
-			const div = createDiv(elo)
-			const leftbar = document.getElementsByClassName("leftbar")[0]
-			leftbar.appendChild(div);
-			//alert(`Ladder : ${getLadderName()}\nElo : ${elo}`)
-		}
-	);
+const getLadderRank = () => {
+	const username = getUserName()
+	console.log(username)
+	if (username) {
+		chrome.runtime.sendMessage( //goes to bg_page.js
+			`https://pokemonshowdown.com/users/${username}`,
+			html => {
+				const regex = RegExp(`<td>${getLadderName()}<\\/td><td style=\"text-align:center\"><strong>(\\d*)<\\/strong>`, "gm")
+				const res = regex.exec(html)
+				const elo = !res ? 0 : res[1]
+
+				const div = createDiv(elo)
+				const leftbar = document.getElementsByClassName("leftbar")[0]
+				leftbar.appendChild(div);
+			}
+		);
+	}
 }
 
 setInterval(() => {
 	const customElem = document.getElementById("showdownLadderInfos")
 	if (customElem === null && window.location.href !== "https://play.pokemonshowdown.com/") {
+		console.log("I'm in a battle")
 		getLadderRank()
 	}
-}, 1000)
+}, 1500)
